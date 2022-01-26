8c65a56d61e03d016a5869ddbe07d7cae89a3d15
Matthew Phillips <phillips.matthew829@gmail.com>
2022-01-26 15:56:05 -0600

    commit tree 573422974f6425e49f256d8c01cc75faad0b36a8

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App(){
+    const [image, setImage] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    // useEffect
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+              console.log(data.message);
+            setImage(data.message);
+            setIsLoaded(true);
+          });
+
+          return(
+              <div>
+          <p>Loading...</p>
+          </div>
+          );
+      }, []); //Only render the fetch request ONCE!
+
+      if (!isLoaded) return <p>Loading...</p>;
+
+    return(
+        <img src={image} alt="A Random Dog" />
+    );
+}
+
+export default App;
